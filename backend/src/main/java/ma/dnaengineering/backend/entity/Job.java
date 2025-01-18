96ebca29634efd322847cc315b2c6ee0ac41f1ee
Youssef Chakir <youssef.chakir@e-polytechnique.ma>
2025-01-18 21:10:25 +0100

    feat: Add dependencies and Job entity
    - Added required dependencies including MySQL support
    - Configured connection properties for MySQL in application.properties
    - Created Job entity with necessary fields and annotations

---
+++ backend/src/main/java/ma/dnaengineering/backend/entity/Job.java
@@ -0,0 +1,46 @@
+package ma.dnaengineering.backend.entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private String location;
+
+    private BigDecimal salary;
+
+    @Column(updatable = false)
+    private LocalDateTime postedAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.postedAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
