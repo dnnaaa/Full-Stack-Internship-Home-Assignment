cedc03ba6b87bedbdc08c170da4430c7e92fa864
Achraf Boutzad <achrafboutzad@gmail.com>
2025-01-19 16:41:56 +0100

    small update in the cros configuration

--- backend/src/main/java/ma/dnaengineering/backend/config/CorsConfig.java
+++ backend/src/main/java/ma/dnaengineering/backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Allow all endpoints
-                        .allowedOrigins("http://localhost:5173") // Allow frontend origin
+                        .allowedOrigins("*") // Allow frontend origin
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow HTTP methods
                         .allowedHeaders("*"); // Allow all headers
             }
