39a2451e24b5c5eefddb7fca7b5e15d25c6055cd
Oussama Okbani <oussama.okbani2013@gmail.com>
2025-01-21 22:15:31 +0100

    redirecting to /jobs

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,7 +1,5 @@
+import { redirect } from 'next/navigation';
+
 export default function HomePage() {
-  return (
-    <div className='flex items-center justify-center h-screen bg-gray-50'>
-      <h1 className='text-4xl font-bold'>Welcome to My Next.js App!</h1>
-    </div>
-  );
+  redirect('/jobs');
 }
