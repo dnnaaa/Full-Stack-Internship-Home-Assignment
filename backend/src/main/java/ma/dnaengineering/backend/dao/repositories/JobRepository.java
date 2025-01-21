03a56a0d8ecf0b0fb4a0d71e4198317b88be9fd3
hemzadev <hamzabenazza.dev@gmail.com>
2025-01-21 23:17:40 +0100

    Update the final version of the back-end

--- backend/src/main/java/ma/dnaengineering/backend/dao/repositories/JobRepository.java
+++ backend/src/main/java/ma/dnaengineering/backend/dao/repositories/JobRepository.java
@@ -2,7 +2,8 @@ package ma.dnaengineering.backend.dao.repositories;
 
 import ma.dnaengineering.backend.dao.entities.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface JobRepository extends JpaRepository<Job, Long> {
+public interface JobRepository extends JpaRepository<Job, Long>, JpaSpecificationExecutor<Job> {
 
 }
